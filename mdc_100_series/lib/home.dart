74b26a8a49ebf239d6ab632376d8272c43a808b4
Bryan Cruz Castillo <bcruzcastillo@hawk.iit.edu>
2020-08-25 22:17:04 -0500

    Add app bar with title and hamburger icon

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -23,12 +23,26 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            }),
+        title: Text('Shrine'),
+        // TODO: Add trailing buttons (102)
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
-      resizeToAvoidBottomInset: false, //doing this ensures that the keyboard's appearance does not alter the size of the home page or its widgets.
+      resizeToAvoidBottomInset:
+          false, //doing this ensures that the keyboard's appearance does not alter the size of the home page or its widgets.
     );
   }
 }
